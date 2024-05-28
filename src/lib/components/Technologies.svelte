7c31ee2eda06e84967ee739c3f50776b34075af5
Heiko Salmon <mail@heiko-salmon.de>
2024-05-28 12:24:26 +0200

    More tools logos

--- src/lib/components/Technologies.svelte
+++ src/lib/components/Technologies.svelte
@@ -6,6 +6,15 @@
   import logo_svelte from '$lib/assets/tools/svelte.svg';
   import logo_tailwind from '$lib/assets/tools/tailwind.svg';
   import logo_vue from '$lib/assets/tools/vue.svg';
+  import logo_php from '$lib/assets/tools/php.svg';
+  import logo_laravel from '$lib/assets/tools/laravel.svg';
+  import logo_nodejs from '$lib/assets/tools/nodejs.svg';
+  import logo_responsive from '$lib/assets/tools/responsive.svg';
+  import logo_wordpress from '$lib/assets/tools/wordpress.svg';
+  import logo_mysql from '$lib/assets/tools/mysql.svg';
+  import logo_mariadb from '$lib/assets/tools/mariadb.svg';
+  import logo_postgresql from '$lib/assets/tools/postgresql.svg';
+  import logo_sqlite from '$lib/assets/tools/sqlite.svg';
 </script>
 
 <section class="">
@@ -14,36 +23,44 @@
       Diese Tools und Technologien setze ich ein.
     </h2>
 
-
-    Standards
+    <h3 class="text-2xl">Standards</h3>
 
     <img src={logo_html5} alt="HTML5"> HTML5
     <img src={logo_css3} alt="CSS3"> CSS3
     <img src={logo_javascript} alt="JavaScript"> Javascript (ES6)
-    responsive / mobile
+    <img src={logo_responsive} alt="Responsive"> responseive / mobile / tablet
 
-    Frontend
+    <h3 class="text-2xl">Frontend</h3>
 
     <img src={logo_svelte} alt="Svelte / SvelteKit"> Svelte / SvelteKit
     <img src={logo_vue} alt="JavaScript"> Vue.js
     <img src={logo_nuxt} alt="Nuxt"> Nuxt
     <img src={logo_tailwind} alt="TailwindCSS"> Tailwind CSS
 
-    Backend
+    <h3 class="text-2xl">Backend</h3>
 
-    Node.js / Express
-    PHP / Laravel
-    Wordpress
-    MySQL / MariaDB / PostgreSQL
-    Ubuntu / Caddy / Nginx / Apache
+    <img src={logo_nodejs} alt="Node.js"> Node.js / Express
+    <img src={logo_php} alt="PHP"> PHP
+    <img src={logo_laravel} alt="Laravel"> Laravel
+    <img src={logo_wordpress} alt="WordPress"> WordPress
+    <img src={logo_mysql} alt="MySQL"> MySQL
+    <img src={logo_mariadb} alt="MariaDB"> MariaDB
+    <img src={logo_postgresql} alt="PostgreSQL"> PostgreSQL
+    <img src={logo_sqlite} alt="SQLite"> SQLite
 
-    Tools
+    <h3 class="text-2xl">Server</h3>
+
+    Ubuntu
+    Caddy
+    Nginx
+    Apache
+
+    <h3 class="text-2xl">Tools</h3>
 
     Vite
     SASS
     Git
     Docker
-
 
   </div>
 </section>

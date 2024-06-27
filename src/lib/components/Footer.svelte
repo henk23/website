47e5e8e502f2421e3b44de992efc9f1f767e1a8a
Heiko Salmon <mail@heiko-salmon.de>
2024-06-27 11:45:30 +0200

    Fix footer padding

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -2,8 +2,8 @@
   const currentYear = new Date().getFullYear();
 </script>
 
-<footer class="text-grey-950 p-4">
-  <div class="container py-8 px-4 font-extrabold text-center">
+<footer class="text-grey-950">
+  <div class="container p-4 font-extrabold text-center">
     &copy; 2011-{currentYear} Heiko Salmon
   </div>
 </footer>

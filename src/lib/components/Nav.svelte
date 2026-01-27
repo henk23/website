68eaf7b2f461c926072c813fe8b6addc7ed44549
Heiko Salmon <mail@heiko-salmon.de>
2026-01-27 09:49:13 +0100

    Upgrade tailwinds

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -2,7 +2,7 @@
   import HSLogo from '$lib/assets/hs-logo.svg?raw';
 </script>
 
-<header class="fixed top-0 z-50 w-full bg-grey-950/70 backdrop-blur-sm text-white font-bold">
+<header class="fixed top-0 z-50 w-full bg-grey-950/70 backdrop-blur-xs text-white font-bold">
   <div class="container px-4 py-2 sm:py-4">
     <nav class="flex items-center justify-between">
       <a href="/" class="block w-20 text-white" aria-label="Zurück nach oben">
